b2dd4c2f258cc853bb20e4d419c7084a0d6ec5b0
Misyani-DS <54836758+Misyani-DS@users.noreply.github.com>
2021-08-31 16:27:10 +0800

    chapter 3 exercise

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,20 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const handbag = {
+  color: 'silver',
+  size: '40 cm',
+  pocketNum: 3,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+
+
+}
+
+console.log("Handbag: ", handbag);
+console.log("Size: ", handbag);
+console.log("Length: Left:"+ handbag.strapLength.left+ " Right: "+handbag.strapLength.right);
